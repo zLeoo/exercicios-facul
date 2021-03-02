9d1a22b2437056bd7ea5770717b71c32307b03a3
Leonardo dos Santos <34231899+zLeoo@users.noreply.github.com>
2021-03-02 11:27:42 -0300

    Add files via upload

---
+++ Exercicio 8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //criação das variáveis
+            float num1, resultado;
+            //entrada de dados
+            Console.WriteLine("Digite um número: ");
+            num1 = float.Parse(Console.ReadLine());
+            //processamento
+            resultado = num1 * num1;
+            //saída
+            Console.WriteLine("O quadrado de " + num1 + " é " + resultado);
+
+
+            Console.ReadKey();
+        }
+    }
+}
