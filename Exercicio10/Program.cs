aabe0705dd46a0c0dfdf3b5e9d5f6a0c9e787ee3
Leonardo dos Santos <34231899+zLeoo@users.noreply.github.com>
2021-03-02 15:00:24 -0300

    Add files via upload

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //criação das variáveis
+            int dias, anos, mes, resultado;
+            //entrada de dados
+            Console.WriteLine("Digite sua idade em dias");
+            dias = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite sua idade em meses");
+            mes = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite sua idade em anos");
+            anos = int.Parse(Console.ReadLine());
+
+            //processamento
+            resultado = (anos * 365) + (mes * 30) + dias;
+
+            //saída
+            Console.WriteLine("Parabéns você já viveu "+ resultado + " dia(s)");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Exercicio9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //criação das variáveis
+            int dias, anos, mes;
+            //entrada de dados
+            Console.WriteLine("Digite sua idade em dias");
+            dias = int.Parse(Console.ReadLine());
+
+
+            //processamento
+            anos = dias / 365;
+            mes = dias % 365 / 30;
+            dias = (dias % 365) % 30;
+
+
+
+            //saída
+            Console.WriteLine("Parabéns você já viveu " + anos + " ano(s)" + "," + mes + " mes(es)" + " e " + dias + " dia(s)") ;
+
+            Console.ReadKey();
+        }
+    }
+}
