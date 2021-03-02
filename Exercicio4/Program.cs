e6ace9859748d5fb83b709a7a97617c0ed8a2b29
Leonardo dos Santos <34231899+zLeoo@users.noreply.github.com>
2021-03-02 00:24:29 -0300

    Add files via upload

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //criação das variáveis
+            float num1, resultado;
+            //entrada de dados
+            Console.WriteLine("Digite uma temperatura em graus celsius: ");
+            num1 = float.Parse(Console.ReadLine());
+            //processamento
+            resultado = (num1 * 9/5) + 32;
+            //saída
+            Console.WriteLine("A temperatura em fahrenheit é " + resultado);
+            Console.ReadKey();
+
+        }
+    }
+}
