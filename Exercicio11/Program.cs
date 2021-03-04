33338fa3fa45b9efa070dafc2c28a4ea639a9bc1
Leonardo dos Santos <34231899+zLeoo@users.noreply.github.com>
2021-03-04 18:13:24 -0300

    Add files via upload

---
+++ Exercicio11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //criação das variáveis
+            float raio, resultado;
+            //entrada de dados
+            Console.WriteLine("Vamos descobrir a área de uma circunferência!");
+            Console.WriteLine("Digite o raio: ");
+            raio = float.Parse(Console.ReadLine());
+
+            //processamento
+            resultado = 3.14159f * (raio * raio);
+
+            //saída
+            Console.WriteLine("A área da circunferência é " + resultado);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ Exercicio12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //criação das variáveis
+            float num1, resultado;
+
+            //entrada de dados
+            Console.WriteLine("Digite um número: ");
+            num1 = float.Parse(Console.ReadLine());
+
+            //processamento
+            resultado = num1 % 6;
+
+            //saída
+            Console.WriteLine("O resto da divisão de "+ num1 + "por 6 é igual a " + resultado);
+            Console.ReadKey();
+        }
+    }
+}
