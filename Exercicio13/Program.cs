0ec56035dc228fe1f62f0ba38475bf09c698c94e
Leonardo dos Santos <34231899+zLeoo@users.noreply.github.com>
2021-03-05 00:06:14 -0300

    Add files via upload

---
+++ Exercicio13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //criação das variáveis
+            float resultado, valor,taxa;
+            int tempo;
+            //entrada de dados
+            Console.WriteLine("Vamos descobrir o valor das suas prestações em atraso!!!");
+            Console.WriteLine("Digite o valor: ");
+            valor = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a taxa: ");
+            taxa = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a digite o tempo: ");
+            tempo = int.Parse(Console.ReadLine());
+
+            //processamento
+            resultado = valor + (valor * (taxa / 100) * tempo);
+            //saída
+            Console.WriteLine("O valor da prestação será R$ "+resultado);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Exercicio14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //criação das variáveis
+            float cotacao, resultado, quantidade;
+            //entrada de dados
+            Console.WriteLine("Digite a cotação do dolar atual");
+            cotacao = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite quantos dolar você tem");
+            quantidade = float.Parse(Console.ReadLine());
+            //processamento
+            resultado = cotacao * quantidade;
+            //saída
+            Console.WriteLine("Você tem "+ resultado + " reais");
+            Console.ReadKey();
+
+        }
+    }
+}
